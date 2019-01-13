ae56e1699f04a05bbaac68de283e92f51ec7a962
Can <canberk8@gmail.com>
2019-01-13 16:13:39 +0300

    view and forms has been added

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth import (authenticate,
                                  login,
-                                 logout
+                                 logoutp
                                  )
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
